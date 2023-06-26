39774913a2d2229b855b8555d3ac896ea867513d
markdhooper <markushooper@gmail.com>
2023-06-26 16:07:55 -0300

    added a clear and restore option to serial45d

---
+++ src/fakeroot/opt/45drives/serial45d/clear_backups.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -f /opt/45drives/serial45d/fru_bin/*
+rm -f /opt/45drives/serial45d/fru_bin_backups/*
+rm -f /opt/45drives/serial45d/fru_ini/*
+rm -f /opt/45drives/serial45d/fru_plaintext_backups/*
